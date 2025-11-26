237e4bf08486843a8b07a3c4b2f34ce415a6ca37
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-11-26 16:45:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let genderState = null;   // 1 = Raja, 0 = Rani
+
+window.onload = () => {
+  // Screens
+  const begin1 = document.getElementById("begin1");
+  const gender = document.getElementById("gender");
+  const intro = document.getElementById("intro");
+  const checkdanger = document.getElementById("checkdanger");
+
+  // Buttons
+  const beginBtn = document.getElementById("beginBtn");
+  const rajaBtn = document.getElementById("rajaBtn");
+  const raniBtn = document.getElementById("raniBtn");
+  const startBtn = document.getElementById("startBtn");
+
+  
+  beginBtn.onclick = () => {
+    begin1.style.display = "none";
+    gender.style.display = "flex";
+  };
+
+  rajaBtn.onclick = () => {
+    genderState = 1;           // ✔ Raja
+    console.log("Gender:", genderState);
+
+    gender.style.display = "none";
+    intro.style.display = "flex";
+  };
+
+  raniBtn.onclick = () => {
+    genderState = 0;           // ✔ Rani
+    console.log("Gender:", genderState);
+
+    gender.style.display = "none";
+    intro.style.display = "flex";
+  };
+  
+  startBtn.onclick = () => {
+
+    intro.style.display = "none";
+    checkdanger.style.display = "flex";
+  };
+};
