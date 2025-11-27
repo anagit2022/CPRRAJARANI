fc5f86e88ffdb2ded7cdb518a3ca5a7400a5a7c2
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-11-27 09:59:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,15 +6,20 @@ window.onload = () => {
   const gender = document.getElementById("gender");
   const intro = document.getElementById("intro");
   const checkdanger = document.getElementById("checkdanger");
-  const introAudio = document.getElementById("introAudio");
-
-
+  const checkresponse = document.getElementById("checkresponse");
+  const checkresponseq = document.getElementById("checkresponseq");
+ const checkbreathing = document.getElementById("checkbreathing");
 
   // Buttons
   const beginBtn = document.getElementById("beginBtn");
   const rajaBtn = document.getElementById("rajaBtn");
   const raniBtn = document.getElementById("raniBtn");
   const startBtn = document.getElementById("startBtn");
+  const dyesBtn = document.getElementById("dyesBtn");
+  const dnoBtn = document.getElementById("dnoBtn");
+  const ryesBtn = document.getElementById("ryesBtn");
+  const rnoBtn = document.getElementById("rnoBtn");
+
 
   
   beginBtn.onclick = () => {
@@ -24,18 +29,16 @@ window.onload = () => {
 
   rajaBtn.onclick = () => {
     genderState = 1;           // ✔ Raja
-  introAudio.play();
-  console.log("Gender:", genderState);
-
+    console.log("Gender:", genderState);
+    introAudio.play();
     gender.style.display = "none";
     intro.style.display = "flex";
   };
 
   raniBtn.onclick = () => {
     genderState = 0;           // ✔ Rani
-    introAudio.play();
     console.log("Gender:", genderState);
-
+    introAudio.play();
     gender.style.display = "none";
     intro.style.display = "flex";
   };
@@ -46,6 +49,25 @@ window.onload = () => {
     checkdanger.style.display = "flex";
     introAudio.pause();
     introAudio.currentTime = 0;
-
   };
+  dyesBtn.onclick = () => {
+
+    checkdanger.style.display = "none";
+    checkresponse.style.display = "flex";
+    // After 10 seconds → go to breathing screen
+  setTimeout(() => {
+    checkresponse.style.display = "none";
+    checkresponseq.style.display = "flex";
+  }, 4000);  // 10000 ms = 10 seconds
+  };
+  rnoBtn.onclick = () => {
+
+    checkresponseq.style.display = "none";
+    checkbreathing.style.display = "flex";
+    
+  };
+  
+
+
+
 };
